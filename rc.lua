660de345380b3fc27c479b7f30d1051f18fa5c25
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2021-06-06 15:58:24 +0200

    Load home directory from environment variables

--- rc.lua
+++ rc.lua
@@ -45,7 +45,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("/home/thomas/.config/awesome/theme.lua")
+beautiful.init(os.getenv("HOME") .. "/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "terminator"
